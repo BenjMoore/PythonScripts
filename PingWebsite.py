9e2cf9fbce0a65daacb07f6344ebf1d69a3dcf60
Benjamin Moore <72554458+BenjMoore@users.noreply.github.com>
2022-02-25 09:56:54 +1000

    v1

---
+++ PingWebsite.py
@@ -0,0 +1,13 @@
+hostname = input("Website >> ") 
+print("Loading...")
+response = os.system("ping -c 1 " + hostname)
+if response == 0:
+	time.sleep(1)
+	print("\n")
+  print("\033[1;35;40mThe Website,[\033\033[0;37;46m",hostname,'\033[1;35;40m] is \033[1;32;40mup!\033')
+else:
+	time.sleep(1)
+	print("\n")
+	print("\033[1;35;40mThe Website,[\033\033[0;37;46m",hostname,'\033[1;35;40m] is\033\033[1;31;40m down!\033')
+  input("\033[1;36;40mPress Enter To Continue...")
+  pass
