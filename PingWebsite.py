6d866a63770f1b91e65c52a48bca7379d0cbdef1
Benjamin Moore <72554458+BenjMoore@users.noreply.github.com>
2022-02-25 10:08:37 +1000

    Update PingWebsite.py

--- PingWebsite.py
+++ PingWebsite.py
@@ -1,13 +1,19 @@
+import sys,os,time
+
 hostname = input("Website >> ") 
 print("Loading...")
 response = os.system("ping -c 1 " + hostname)
 if response == 0:
-	time.sleep(1)
-	print("\n")
-  print("\033[1;35;40mThe Website,[\033\033[0;37;46m",hostname,'\033[1;35;40m] is \033[1;32;40mup!\033')
+    time.sleep(1)
+    print("\n")
+    print("\033[1;35;40mThe Website,[\033\033[0;37;46m",hostname,'\033[1;35;40m] is \033[1;32;40mup!\033')
+    input("\033[1;36;40mPress Enter To Continue...")
+    sys.exit()
+        
 else:
-	time.sleep(1)
-	print("\n")
-	print("\033[1;35;40mThe Website,[\033\033[0;37;46m",hostname,'\033[1;35;40m] is\033\033[1;31;40m down!\033')
-  input("\033[1;36;40mPress Enter To Continue...")
-  pass
+    time.sleep(1)
+    print("\n")
+    print("\033[1;35;40mThe Website,[\033\033[0;37;46m",hostname,'\033[1;35;40m] is\033\033[1;31;40m down!\033')
+    input("\033[1;36;40mPress Enter To Continue...")
+    sys.exit()
+  
